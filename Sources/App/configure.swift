e57ad3cce020e75082876cf6429275ae519d174f
João Pedro Leite <joaopleite26@gmail.com>
2021-06-16 12:32:44 -0300

    changing to postgres since sqlite is not heroku friendly

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,6 @@
 import Vapor
 import Fluent
-import FluentSQLiteDriver
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -10,9 +10,11 @@ public func configure(_ app: Application) throws {
     // register routes
     try routes(app)
     
-    app.databases.use(.sqlite(.memory), as: .sqlite)
-    app.migrations.add(GameDeal(), to: .sqlite)
-    try app.autoMigrate().wait()
+    var configuration = PostgresConfiguration(url: Environment.get("DATABASE_URL")!)
+    configuration!.tlsConfiguration?.certificateVerification = .fullVerification
+    try app.databases.use(.postgres(configuration: configuration!), as: .psql)
     
+    app.migrations.add(GameDeal(), to: .psql)
+    try app.autoMigrate().wait()
     try XboxDataBusiness().populateDatabase(fromApp: app)
 }
